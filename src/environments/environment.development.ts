64a2a3a61cc1fde963741261543ed04674f06024
Jamez <adobeclip2003@gmail.com>
2025-05-02 08:23:39 +0100

    UPDATE: Making smoe payment integration changes

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    base_backend : {
-        url : ''
-    }
-};
--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: true,
-    base_backend : {
-        url : ''
-    },
-    flutterwave: {
-        publick_key: ''
-    }
-};
